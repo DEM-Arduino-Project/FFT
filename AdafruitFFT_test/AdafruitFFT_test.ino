d583c2caa14841e95b6c7c23d12ae6f8cbd704c9
Paul Hondola <paulhondola@gmail.com>
2024-04-15 19:31:37 +0300

    adafruit fft zero implementation

--- AdafruitFFT_test/AdafruitFFT_test.ino
+++ AdafruitFFT_test/AdafruitFFT_test.ino
@@ -16,28 +16,52 @@
 #define DATA_SIZE 256
 
 //the sample rate
-#define FS 8000
+#define SAMPLING_RATE 8000
+#define CHANNEL A0
+
+#define USE_HARD_CODED_DATA 1
+
+int analog_signal_data[DATA_SIZE];
 
 // the setup routine runs once when you press reset:
 void setup() {
+
   Serial.begin(115200);
   while(!Serial); //wait for serial to be ready
 
   //run the FFT
-  ZeroFFT(signal, DATA_SIZE);
+  if(USE_HARD_CODED_DATA)
+    ZeroFFT(hard_coded_signal_data, DATA_SIZE); 
+  else
+    ZeroFFT(analog_signal_data, DATA_SIZE);
 
   //data is only meaningful up to sample rate/2, discard the other half
   for(int i=0; i<DATA_SIZE/2; i++){
     
     //print the frequency
-    Serial.print(FFT_BIN(i, FS, DATA_SIZE));
+    Serial.print(FFT_BIN(i, SAMPLING_RATE, DATA_SIZE));
     Serial.print(" Hz: ");
 
     //print the corresponding FFT output
-    Serial.println(signal[i]);
+    if(USE_HARD_CODED_DATA)
+      Serial.println(hard_coded_signal_data[i]);
+    else
+      Serial.println(analog_signal_data[i]);
+    
   }
 }
 
 void loop() {
   //don't even do anything
 }
+
+void read_signal_data()
+{
+  for(int i=0; i<DATA_SIZE; i++)
+  {
+    // read data from pin A0
+    int sensor_read = analogRead(CHANNEL);
+    analog_signal_data[i] = (sensor_read + 0.5) * (5.0 / 1023.0);
+  }
+}
+
